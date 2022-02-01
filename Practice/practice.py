36c2e6b7321dfd9084f1e4c88d285644fc0c0cb3
echoaj <aljoslin13@yahoo.com>
2022-02-01 09:46:02 -0800

    practice matthew

--- Practice/practice.py
+++ Practice/practice.py
@@ -1,47 +1,35 @@
+# Parent class, Base class, Super class
+class Employee:
+    def __init__(self, fn, ln, ag, em, ssn):
+        self.firstName = fn
+        self.lastName = ln
+        self.age = ag
+        self.email = em
+        self.SSN = ssn
+
+    def display(self):
+        print()
+        print(self.firstName, self.lastName)
+        print(f"AGE: {self.age} Email: {self.email}")
+        print(f"SSN: {self.SSN}")
 
 
-def addup(limit, x, y, total):
-    if total == limit:
-        return True
-    elif total > limit:
-        return False
-    else:
-        result1 = addup(limit, x, y, total+x)
-        result2 = addup(limit, x, y, total+y)
-        return result1 or result2
+# Sub class, Child class
+class Manager(Employee):
+    def __init__(self, fn, ln, ag, em, ssn, dep, rf):
+        super(Manager, self).__init__(fn, ln, ag, em, ssn)
+        self.department = dep
+        self.resp_for = rf
 
-print(addup(11, 2, 3, 0))
-print(addup(11, 2, 3, 0))
-
-
-def print_nums(i, limit):
-    if i > 10:
-        return
-    else:
-        print(i)
-        print_nums(i+1, limit)
-
-print_nums(1, 10)
+    # overriding
+    def display(self):
+        super(Manager, self).display()
+        print(self.department, self.resp_for)
+        print()
 
 
-# return a list of all numbers that when multiplied
-def mult_combos(limit, x, y, combos):
-    # print(x,y)
-    if x > limit:
-        return combos
-    if y > limit:
-        return
-    if x*y == limit and [x,y] not in combos:
-        combos.append([x,y])
-        return
-    mult_combos(limit, x, y+1, combos)
-    return mult_combos(limit, x+1, y, combos)
+emp = Employee("Alex", "Joslin", 26, "aj@gmail.com", 123456789)
+emp.display()
 
-# print(mult_combos(12, 0, 0, []))
-
-num = 52
-strnum = str(num)
-char_array = [int(c) for c in strnum]
-total = sum(char_array)
-print(total)
-
+mng = Manager("Lisa", "Janis", 56, "aj@gmail.com", 123456789, "software", 15)
+mng.display()   # polymorphism
