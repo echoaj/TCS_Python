09fa1625fbe0e08fafa61518bb927f5fed032d75
echoaj <aljoslin13@yahoo.com>
2022-05-28 11:11:16 -0700

    practice updated for bfs

--- Practice/practice.py
+++ Practice/practice.py
@@ -1,5 +1,22 @@
 
+queue = []
 visited = []
+
+
+def breadth_first_search(x, y):
+    queue.append((x, y))
+    visited.append((x, y))
+    while queue:
+        x, y = queue.pop(0)
+        #             right      down      left       up
+        neighbors = [(x+1, y), (x, y+1), (x-1, y), (x, y-1)]
+        for x, y in neighbors:
+            if (x, y) not in visited and matrix[x][y] != 0:
+                queue.append((x, y))
+                visited.append((x, y))
+
+
+# matrix of zeros
 matrix = [[0, 0, 0, 0, 0, 0],
           [0, 1, 0, 3, 4, 0],
           [0, 5, 6, 7, 8, 0],
@@ -7,33 +24,7 @@ matrix = [[0, 0, 0, 0, 0, 0],
           [0, 13, 14, 15, 16, 0],
           [0, 0,   0,  0,  0, 0]]
 
-solvable = False
-
-
-def dfs(matrix, x, y):
-    global solvable
-
-    if solvable:
-        return solvable
-    if matrix[x][y] == 16:
-        return True
-    if matrix[x][y] == 0:
-        return False
-    if matrix[x][y] in visited:
-        return False
-    else:
-        visited.append(matrix[x][y])
-
-    left =  dfs(matrix, x + 1, y)   # check right neighbor
-    right = dfs(matrix, x, y + 1)   # check down neighbor
-    down =  dfs(matrix, x + 1, y)   # check left neighbor
-    up =    dfs(matrix, x, y - 1)   # check up neighbor
-
-    solvable = left or right or down or up
-    return solvable
-
-
-result = dfs(matrix, 1, 1)
-print(result)
-
+breadth_first_search(1, 1)
 print(visited)
+for x, y in visited:
+    print(matrix[x][y])
