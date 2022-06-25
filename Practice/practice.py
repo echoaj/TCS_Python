175f7c4771abc9afed7d47c9467bcccb03f67f55
echoaj <aljoslin13@yahoo.com>
2022-06-25 14:02:48 -0700

    longest pal

--- Practice/practice.py
+++ Practice/practice.py
@@ -1,46 +1,12 @@
 
 
-graph = {'A': ['B', 'C', 'D'],
-         'B': ['A', 'G'],
-         'G': ['B'],
-         'C': ['A', 'E', 'H'],
-         'D': ['A', 'E'],
-         'E': ['C', 'D', 'F', 'H', 'I'],
-         'F': ['E'],
-         'H': ['C', 'E', 'J'],
-         'I': ['E'],
-         'J': ['H']
-        }
+# Longest Palindromic Substring
+# Create algorithm to return the longest palindomic substring
 
-visted = []
+string = "abbcdnekensld"       # "neken" -> 5
 
 
-# create a dfs recursive function
-def dfs(node):
-    visted.append(node)
-    for n in graph[node]:
-        if n not in visted:
-            dfs(n)
+count = 0
+for m in range(len(string)):
+    pass
 
-
-# 1) stop recursion when node is reached
-# 2) return true if solvable else return false
-def dfs_modified(node, end):
-    if node == end:
-        visted.append(node)
-        return True
-    visted.append(node)
-    for n in graph[node]:
-        if n not in visted:
-            solved = dfs_modified(n, end)
-            if solved:
-                return True
-    return False
-
-
-dfs('A')
-print(visted)
-visted.clear()
-solvable = dfs_modified('A', 'F')
-print(visted)
-print(solvable)
