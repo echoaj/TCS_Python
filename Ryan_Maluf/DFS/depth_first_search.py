f7040ad93511deb3ce30c0a3a536618e62b1b041
echoaj <aljoslin13@yahoo.com>
2022-07-22 12:47:08 -0700

    finished dfs

---
+++ Ryan_Maluf/DFS/depth_first_search.py
@@ -0,0 +1,42 @@
+
+
+graph = {
+    "A": ["B", "C", "D"],
+    "B": ["A", "G"],
+    "C": ["A", "E", "H"],
+    "D": ["A", "E"],
+    "E": ["C", "D", "F", "H", "I"],
+    "F": ["E"],
+    "G": ["B"],
+    "H": ["C", "E", "J"],
+    "I": ["E"],
+    "J": ["H"]
+}
+
+visited = []
+path = []
+
+
+def dfs(node):
+    visited.append(node)
+    for n in graph[node]:
+        if n not in visited:
+            dfs(n)
+
+
+def dfs_mod(node, end):
+    path.append(node)
+    if node == end:
+        return True
+    for n in graph[node]:
+        if n not in path:
+            if dfs_mod(n, end):
+                return True
+
+
+dfs("A")
+print(visited)
+
+solvable = dfs_mod("A", "D")
+print(path)
+print(solvable)
