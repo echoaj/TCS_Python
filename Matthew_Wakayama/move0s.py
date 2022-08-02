fac26bb61120c819dcdc6cd6d83433a5db44e810
echoaj <aljoslin13@yahoo.com>
2022-08-02 16:31:41 -0700

    matthew lesson

--- Matthew_Wakayama/move0s.py
+++ Matthew_Wakayama/move0s.py
@@ -9,3 +9,14 @@ array = [1, 2, 0, 4, 0, 0, 7, 0, 9]         # [1, 2, 4, 7, 9, 0, 0, 0, 0]
 # 2) If the right pointer is at 0 only move the right.  Otherwise, move both right and left
 # 3) When the right pointer find a non 0 switch left and right pointer.
 
+l = 0
+r = 0
+while r < len(array):
+    if array[r] == 0:
+        r += 1
+    else:
+        array[l], array[r] = array[r], array[l]
+        r += 1
+        l += 1
+
+print(array)
--- Practice/practice2.py
+++ Practice/practice2.py
@@ -1,27 +1,8 @@
 
-#         01234567
-word = "abbcdbbdcdakj"     # cdbdc 5
+import json
 
+# import json and read allergies.json
+with open('allergies.json', 'r') as json_file:
+    allergy_types = json.load(json_file)
 
-def getLongestPal(string):
-    longest = 0
-    for m in range(len(string)):
-        # Works odd number palindromes
-        l = m
-        r = m
-        while string[l] == string[r] and l >= 0 and r < len(string)-1:
-            longest = max(longest, r - l + 1)
-            l -= 1
-            r += 1
-        # Works even number palindromes
-        l = m
-        r = m+1
-        while string[l] == string[r] and l >= 0 and r < len(string):
-            longest = max(longest, r - l + 1)
-            l -= 1
-            r += 1
-
-    return longest
-
-
-print(getLongestPal(word))
+print(allergy_types)
