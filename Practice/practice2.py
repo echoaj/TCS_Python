5bd695e234ff4ef3124f7042a4f5e82f7bfda0a6
echoaj <aljoslin13@yahoo.com>
2022-08-17 17:27:19 -0700

    linked list lesson

--- Practice/practice2.py
+++ Practice/practice2.py
@@ -1,8 +1,37 @@
 
-import json
+# Doubly linked list
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+        self.prev = None
 
-# import json and read allergies.json
-with open('allergies.json', 'r') as json_file:
-    allergy_types = json.load(json_file)
 
-print(allergy_types)
+class DoublyLinkedList:
+    def __init__(self, val):
+        self.head = Node(val)
+        self.tail = self.head
+
+    def append(self, val):
+        self.tail.next = Node(val)
+        self.tail.next.prev = self.tail
+        self.tail = self.tail.next
+
+    def display(self):
+        if self.head is None:
+            print("List is empty")
+            return
+        else:
+            temp = self.head
+            while temp is not None:
+                print(temp.data, end=" <->")
+                temp = temp.next
+            print()
+
+
+dll = DoublyLinkedList(5)
+dll.append(10)
+dll.append(10)
+dll.append(10)
+dll.append(10)
+dll.display()
